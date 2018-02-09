2b52bbf1b02892a09b7fb32d5a1c22f7babbb63d
Joseph Donati <josephdonati@csus.edu>
2018-02-09 09:18:01 -0800

    Testing commit for first time

---
+++ bin/Sprint 1 Design Notes.md
@@ -0,0 +1,17 @@
+﻿
+# Sprint 1 - Design
+### Design Choices
+- I tried to decompose the program with methods to handle almost every action instead of coding more things together. This lead to more method calls, and was inspired by Dr. Posnett's advice in class. I would typically repeat more coding, with less decomposed nature. I can't honestly say I had a good reason for doing this other than to follow the professor's advice, but after doing so, can clearly see the benefit. 
+- Could have combined cmdStart and cmdStop methods, but left separate for clarity, and possibility of needing to modify later.
+- Used FileWriter with a PrintWriter to write individual lines to log file, Scanner to read back.
+- To handle the reading of the log file for the summary I came up with a solution which reads each line of text from the log into a StringTokenizer. Each token is then then added to an ArrayList so that the tokens can be accessed individually for comparison or subtraction (calculating duration). Each ArrayList would then represent a single line of the log, and each ArrayList would be stored in a Linked list. This would allow comparison between elements, between lines. With the LinkedList of ArrayLists, I was envisioning a sort of 2D array which would allow searching and access for individual tokens in the log file.
+- I separated the tokens in each line in the log with a slash, "/", so I could use the StringTokenizer to separate each portion of the line, while keeping the description Strings intact
+### Limitations / Concerns
+- I ran out of time before being able to finish this assignment. This was caused by two things, underestimating the complexity and time requirements of the project, and waiting until it was too late to start working. Life has been hectic (wife and I have the flu, newborn baby, etc., etc.,), but I will work to not let this happen again, namely by starting assignments as soon as they are assigned.
+- Summary methods were unfinished and untested. 
+- I had not yet devised a solution to calculate duration from my LocalDateTime time stamps in the log, not sure the LocalDateTime would have ended up working, or would have had to calculate duration manually, parsing the time codes to ints and doing the math myself
+- I had not yet devised a way to combine individual task summaries to overall summary.
+- Can only accept specific input, e.g. "start" not "Start". I'm accepting this because the users are programmers, and command line input is typically case-sensitive. 
+- I have a lot of IOExceptions suggested by Eclipse, necessary to make the project run correctly, which I don't fully understand. I get the concept of the required exceptions, with each calling method requiring an exception "up the chain". They seem excessive, though, and I feel like there is a more elegant solution which I am not seeing. 
+
+> Written with [StackEdit](https://stackedit.io/).
---
+++ src/TM.java
@@ -0,0 +1,247 @@
+//dir for CD in command line//  C:\Users\josep\Google Drive\School\SPRING 2018\Software Engineering CSC-131\Time Manag\src
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/**
+ * The TM class allows a user to track time worked on multiple tasks, and print summaries of the time worked.
+ * 
+ * Joseph Donati - CSUS - CSC131-03 - Sprint 1 - 2/4/2018 - Professor D. Posnett
+ */
+public class TM {
+	// main only used to pass args to appMain, gets rid of static issues
+	public static void main(String [] args) throws IOException {
+		TM tm = new TM();
+		tm.appMain(args);
+	}
+	
+	// appMain is where program actually runs, not static
+	void appMain(String [] args) throws IOException {
+		
+		// Declare variables
+		String cmd;
+		String taskName;
+		String description;
+		
+		// Create TaskLog to interact with persistant log
+		TaskLog log = new TaskLog();
+		
+		// Create object to pull time codes
+		LocalDateTime currentTime = LocalDateTime.now();
+		
+		// Rule out unacceptable usage, if incorrect display usage instructions
+		try {		
+			cmd = args[0];
+			if (args.length < 2 && cmd.equals("summary"))
+				taskName = null;
+			else 
+				taskName = args[1];
+			if (args[0].equals("describe")) {
+					description = args[2];
+			}
+			else 
+				description = null;		
+		}
+		catch (ArrayIndexOutOfBoundsException ex) {
+			// Usage instructions
+			System.out.println("Usage:");
+			System.out.println("\tTM start <task name>");
+			System.out.println("\tTM stop <task name>");
+			System.out.println("\tTM describe <task name> <task description in quotes>");
+			System.out.println("\tTM summary <task name>");
+			System.out.println("\tTM summary");
+			return;
+		}
+		
+		// switch to direct operation
+		switch (cmd) {
+			case "stop": cmdStop(taskName, log, cmd, currentTime);
+						break;
+			case "start": cmdStart(taskName, log, cmd, currentTime);
+						break;
+			case "describe": cmdDescribe(taskName, log, cmd, currentTime, description);
+						break;
+			case "summary": if (taskName == null)
+								cmdSummary(log);
+							else
+								cmdSummary(log, taskName);
+						break;
+		}
+		
+	}
+	
+	
+	/**
+	 * cmdStart method prints start entry to log
+	 * @param taskName The name of the task
+	 * @param log The TaskLog object wrapping the persistent log
+	 * @param cmd The command supplied by the user
+	 * @param currentTime The current time at log entry
+	 * @throws IOException
+	 */
+	void cmdStart(String taskName, TaskLog log, String cmd, LocalDateTime currentTime) throws IOException {
+		String line = (currentTime + "/" + taskName + "/" + cmd);
+		log.writeLine(line);
+	}
+	
+	/**
+	 * cmdStop method prints stop entry to log
+	 * @param taskName The name of the task
+	 * @param log The TaskLog object wrapping the persistent log
+	 * @param cmd The command supplied by the user
+	 * @param currentTime The current time at log entry
+	 * @throws IOException
+	 */
+	void cmdStop(String taskName, TaskLog log, String cmd, LocalDateTime currentTime) throws IOException {
+		String line = (currentTime + "/" + taskName + "/" + cmd);
+		log.writeLine(line);
+	}
+	
+	/**
+	 * cmdDescribe method prints describe entry to log
+	 * @param taskName The name of the task
+	 * @param log The TaskLog object wrapping the persistent log
+	 * @param cmd The command supplied by the user
+	 * @param currentTime The current time at log entry
+	 * @param description The description of the task provided by the user
+	 * @throws IOException
+	 */
+	void cmdDescribe(String taskName, TaskLog log, String cmd, LocalDateTime currentTime, String description) throws IOException {
+		String line = (currentTime + "/" + taskName + "/" + cmd + "/" + description);
+		log.writeLine(line);
+	}
+	
+	//NOTE: THIS METHOD UNFINISHED
+	/**
+	 * The cmdSummary method prints a summary of all tasks in log
+	 * @param log The TaskLog object wrapping the persistent log
+	 * @throws FileNotFoundException
+	 */
+	void cmdSummary(TaskLog log) throws FileNotFoundException {
+		//System.out.println("WHOLE SUMMARY");
+		LinkedList<ArrayList<String>> allLines = log.readFile();
+		java.util.Iterator<ArrayList<String>> it = allLines.iterator();
+		while(it.hasNext())
+			System.out.println(it.next());        // For testing   
+		
+	}
+	
+	//NOTE: THIS METHOD UNFINISHED
+	/**
+	 * The cmdSummary method prints a summary of a task from log
+	 * @param log The TaskLog object wrapping the persistent log
+	 * @param task The individual task to provide summary for
+	 * @throws FileNotFoundException
+	 */
+	void cmdSummary(TaskLog log, String task) throws FileNotFoundException {
+		//System.out.println("SINGLE SUMMARY");
+		String descript = null;
+		LinkedList<ArrayList<String>> allLines = log.readFile();
+		
+		// Get description
+		for (int i = 0; i < allLines.size(); i++) {
+			if (allLines.get(i).get(1).equals(task) && allLines.get(i).get(2).equals("describe"))
+				descript = allLines.get(i).get(3);
+		}
+		
+		// Get duration (Was in progress, ran out of time beforw I could make this work)
+		LocalDateTime start, finish, duration;
+		for (int i = 0; i < allLines.size(); i++) {
+			if (allLines.get(i).get(1).equals(task) && allLines.get(i).get(2).equals("start"))
+				start = LocalDateTime.parse(allLines.get(i).get(0));	
+		}
+		for (int i = 0; i < allLines.size(); i++) {
+			if (allLines.get(i).get(1).equals(task) && allLines.get(i).get(2).equals("stop"))
+				finish = LocalDateTime.parse(allLines.get(i).get(0));
+		}
+		//start = get(
+		//duration = finish.minus(start);
+		
+		// Print summary
+		System.out.println("\nSummary for task:\t|  " + task);
+		System.out.println("Description:\t\t|  " + descript);
+		System.out.println("Duration:\t\t|  (TIME)");
+		
+	}
+	
+	
+	
+	/**
+	 * The TaskLog class has methods for writing and reading from a persistent log file
+	 */
+	public class TaskLog {
+		
+		/*Reference note* Needed a refresher on file I/O, used method from: Starting out with Java, 2nd Edition, Gaddis + Muganda, print*/
+		
+		// Create FileWriter and PrintWriter to write to log file
+		public FileWriter fwriter;
+		public PrintWriter outputFile;
+		
+		
+		/**
+		 * Constructor, creates file I/O objects
+		 */
+		public TaskLog() throws IOException{
+			fwriter = new FileWriter("TMlog.txt", true);
+			outputFile = new PrintWriter(fwriter);
+		}
+		
+		/**
+		 * The writeLine method writes a line to the persistent log file
+		 * @param line The line to write to the log file
+		 * @throws IOException
+		 */
+		void writeLine(String line) throws IOException{
+			outputFile.println(line);
+			/* System.out.println("TESTHERE, line: " + line); // Testing: Uncomment to see the line being written to log file in console */
+			outputFile.close();
+		}		
+		
+		/**
+		 * The readFile method reads a persistent log file into the program
+		 * @return LinkedList holding ArrayList corresponding to each line in the log, with each token in the line stored as an element of the ArrayList
+		 * @throws FileNotFoundException
+		 */
+		LinkedList<ArrayList<String>> readFile() throws FileNotFoundException {
+			
+			// Create LinkedList of ArrayList of Strings to hold each token in each line
+			LinkedList<ArrayList<String>> lineList = new LinkedList<ArrayList<String>>();
+			
+			// Open file for reading
+			File logFile = new File("TMlog.txt");
+			Scanner inputFile = new Scanner(logFile);
+			
+			// Process each line in the string
+			String inLine;
+			while(inputFile.hasNextLine()) {
+				
+				// Create an ArrayList for each line in the log
+				ArrayList<String> eachLine = new ArrayList<String>();
+				inLine = inputFile.nextLine();
+				
+				// Add each token in a line to its own element of the ArrayList
+				StringTokenizer st = new StringTokenizer(inLine, "/");
+				while (st.hasMoreTokens()) {
+					eachLine.add(st.nextToken());
+				}
+				
+				// Add the ArrayList to the LinkedList
+				lineList.add(eachLine);
+				
+				/*for (String tkn : eachLine)
+					System.out.println(tkn + "#");           // For testing purposes*/ 
+			}
+			
+			return lineList;
+		}
+	}
+}
+
